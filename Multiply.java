5584cbbe48949f66e09b5ef26fc43a4a8248115c
shubh4813 <83203524+shubh4813@users.noreply.github.com>
2021-04-28 09:20:26 +0530

    add and mul

--- Multiply.java
+++ Multiply.java
@@ -5,3 +5,12 @@ System.out.println("Multiplication of given no. is ", +mul);
 		}
 
 }
+
+
+public class Multiply{
+	public static void main(String args[]){
+		int a=2,b=4,mul=0;
+System.out.println("Multiplication of given no. is ", +mul);
+		}
+
+}
